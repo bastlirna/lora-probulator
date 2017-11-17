1d3c660d06b590480e1c7907f2f621db590501b0
ah01 <horcicaa@gmail.com>
2017-11-17 12:45:33 +0100

    More description

--- fw/src/imgs/combine.ps1
+++ fw/src/imgs/combine.ps1
@@ -1,19 +1,21 @@
+# Generate icons.h
+#
+# Warning: This script is call by compiler (see platform.ini) and cannot have any output
+#          Otherwise output will be used as build flags and probably brake build!
+
 Push-Location $PSScriptRoot
 
 # generate XBM from BMP images (require ImageMagic in path)
 magick mogrify -format xbm *.bmp
 
-# 
+# combine all *.xbm to one file icons.h
 $icons = "";
-
 Get-ChildItem -filter *.xbm | ForEach-Object {
-    #Write-Host "Loading $_"
     $content = Get-Content $_.FullName -Raw
     $icons = $icons + "`n" + $content
 }
 
 $icons = $icons -replace "_bits\[\] ", "_bits[] PROGMEM "
-
 $icons | Set-Content "icons.h"
 
 Pop-Location
