6b021bbb468f9c87195d2a09b7759b642db15623
ah01 <horcicaa@gmail.com>
2017-11-17 14:24:41 +0100

    Add tool for mirror screen on PC (show and save images)

---
+++ tools/ScreenMirror/MainWindow.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(119, 97);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(148, 84);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM18";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Filter = "png (*.png)|";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(279, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(391, 241);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "LoRa Probulator - Screen Mirror";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ tools/ScreenMirror/MainWindow.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap buffer = null;
+        Stopwatch sw = new Stopwatch();
+
+        StringBuilder sb = new StringBuilder();
+        double fps;
+
+        bool pause = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            updateButton();
+            
+            serialPort1.Open();
+            sw.Start();
+        }
+
+        void updateButton()
+        {
+            button1.Text = pause ? "Run" : "Stop";
+            label1.Text = pause ? "Stoped" : "";
+        }
+
+
+        void Draw()
+        {
+            var data = sb.ToString().Trim(); 
+            if (string.IsNullOrEmpty(data))
+            {
+                sb.Clear();
+                return;
+            }
+
+            byte[] raw;
+
+            try
+            {
+                raw = data.Split(' ').Select(x => FromHex(x)).ToArray();
+            }
+            catch (Exception x)
+            {
+                Debug.Print(x.ToString());
+                sb.Clear();
+                return;
+            }
+
+            if (raw.Length != 1024)
+            {
+                Debug.Print("invalid data");
+                sb.Clear();
+                return;
+            }
+
+            var b = new Bitmap(128, 64);
+
+            for (var y = 0; y < 64 / 8; y++)
+            {
+                for (var x = 0; x < 128; x++)
+                {
+                    var f = raw[x + y * 128];
+
+                    for (var i = 0; i < 8; i++)
+                    {
+                        var p = (f & 1 << i) != 0;
+                        Color c;
+
+                        if (p)
+                        {
+                            c = Color.Aqua;
+                        }
+                        else
+                        {
+                            c = Color.Black;
+                        }
+
+                        b.SetPixel(x, y * 8 + i, c);
+                    }
+                }
+            }
+
+
+            Debug.WriteLine(data.Length / 2);
+
+
+            buffer = b;
+
+            sb.Clear();
+
+
+            fps = 1000d / sw.ElapsedMilliseconds;
+            
+            sw.Restart();
+
+            panel1.Invalidate();
+
+        }
+
+        private static byte FromHex(string x)
+        {
+            return (byte)Convert.ToUInt32(x, 16);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (buffer == null) return;
+
+            e.Graphics.DrawImage(buffer, 10, 10);
+            label1.Text = $"{fps:F2} FPS";
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            
+
+            while(serialPort1.BytesToRead > 0)
+            {
+                var b = serialPort1.ReadChar();
+
+                if (pause) continue;
+
+                if (b == '\r') continue;
+
+                if (b == '\n')
+                {
+                    Draw();
+                    continue;
+                }
+
+                sb.Append((char)b);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pause = !pause;
+            updateButton();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var file = saveFileDialog1.FileName;
+
+                Debug.Print(file);
+
+                buffer.Save(file);
+            }
+        }
+    }
+}
